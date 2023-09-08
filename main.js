5c42039ccd3c8af3d6c42880c3dc1f36d307af71
ortizmanuela <141923993+ortizmanuela@users.noreply.github.com>
2023-09-08 08:52:24 -0300

    Upload do site

---
+++ main.js
@@ -0,0 +1,89 @@
+var button = document.querySelector('.botao');
+var card_texto = document.querySelector ('.card_texto');
+
+button.addEventListener('click', function(){
+
+    if(card_texto.style.display==='block') {
+        card_texto.style.display = 'none';
+    } else {
+        card_texto.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao2');
+var card_texto2 = document.querySelector ('.card_texto2');
+
+button.addEventListener('click', function(){
+
+    if(card_texto2.style.display==='block') {
+        card_texto2.style.display = 'none';
+    } else {
+        card_texto2.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao3');
+var card_texto3 = document.querySelector ('.card_texto3');
+
+button.addEventListener('click', function(){
+
+    if(card_texto3.style.display==='block') {
+        card_texto3.style.display = 'none';
+    } else {
+        card_texto3.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao4');
+var card_texto4 = document.querySelector ('.card_texto4');
+
+button.addEventListener('click', function(){
+
+    if(card_texto4.style.display==='block') {
+        card_texto4.style.display = 'none';
+    } else {
+        card_texto4.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao5');
+var card_texto5 = document.querySelector ('.card_texto5');
+
+button.addEventListener('click', function(){
+
+    if(card_texto5.style.display==='block') {
+        card_texto5.style.display = 'none';
+    } else {
+        card_texto5.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao6');
+var card_texto6 = document.querySelector ('.card_texto6');
+
+button.addEventListener('click', function(){
+
+    if(card_texto6.style.display==='block') {
+        card_texto6.style.display = 'none';
+    } else {
+        card_texto6.style.display = 'block';
+}
+});
+
+
+var button = document.querySelector('.botao7');
+var card_texto7 = document.querySelector ('.card_texto7');
+
+button.addEventListener('click', function(){
+
+    if(card_texto7.style.display==='block') {
+        card_texto7.style.display = 'none';
+    } else {
+        card_texto7.style.display = 'block';
+}
+});
